7f77db92e386425c56c5db90b6f48134aec15cf8
sungsuhan <swhanssu@gmail.com>
2022-09-15 17:19:44 +0900

    class

--- ts/tsStudy.ts
+++ ts/tsStudy.ts
@@ -3,13 +3,13 @@ type Name = string;
 type Age = number;
 
 // optional
-type Player = {
+type Player1 = {
     name: Name,
     age?: Age
 };
 
 // 인자 타입 선언
-const playerMaker = (name:string) : Player => ({ name });
+const playerMaker = (name:string) : Player1 => ({ name });
 const sungsu = playerMaker("sungsu")
 sungsu.age = 29;
 
@@ -99,3 +99,74 @@ const messi:Man<null> = {
     name: "messi",
     extraInfo: null
 }
+
+// class, abstract class(추상클래스)
+abstract class User {
+    constructor(
+        private firstName:string,
+        private lastName:string,
+        public nickname:string
+    ) {}
+    getFullName() {
+        return `${this.lastName} ${this.firstName}`
+    }
+}
+
+class Player extends User{}
+
+const han = new Player("ss", "han", "hss");
+
+han.getFullName();
+
+// 복습 - 사전
+type Words = {
+    [key:string]:string
+}
+
+class Dict {
+    private words: Words
+    constructor(){
+        this.words = {}
+    }
+    add(word:Word){
+        if(this.words[word.term] === undefined){
+            this.words[word.term] = word.def;
+        }
+    }
+    def(term:string){
+        return this.words[term];
+    }
+    del(term:string){
+            delete this.words[term];
+    }
+    upd(term:string, def:string){
+        if(this.words[term]){
+            this.words[term] = def;
+        }
+    }
+}
+
+class Word {
+    constructor(
+        public term:string,
+        public def:string,
+    ) {}
+}
+
+const kimchi = new Word("kimchi", "한국 음식");
+const pizza = new Word("pizza", "이탈리아 음식");
+const sushi = new Word("sushi", "일본 음식");
+
+const dict = new Dict();
+
+dict.add(kimchi);
+dict.add(pizza);
+dict.add(sushi);
+
+dict.def("kimchi");
+dict.def("pizza");
+dict.def("sushi");
+
+dict.del("kimchi")
+dict.upd("kimchi","매움");
+
